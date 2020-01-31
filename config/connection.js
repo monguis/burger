8b9a91c1a6f98650877da216ee696fbe9ef2e171
monguis <jccnnetwork@gmail.com>
2020-01-31 12:49:13 -0700

    fixing conection to database

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,30 @@
 const mysql = require("mysql");
-const connection = mysql.createConnection({
-    host: "localhost",
-    password: "password",
-    database: "burgers_db",
-    port: 3306,
-    user: "root"
-})
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     password: "password",
+//     database: "burgers_db",
+//     port: 3306,
+//     user: "root"
+// })
 
 
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-connection.connect((err) => {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-})}
+if (process.env.JAWSDB_URL) {
+   const connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+            host: "localhost",
+            password: "password",
+            database: "burgers_db",
+            port: 3306,
+            user: "root"
+        })
+}
+    connection.connect((err) => {
+        if (err) {
+            console.error("error connecting: " + err.stack);
+            return;
+        }
+        console.log("connected as id " + connection.threadId);
+    })
 
 module.exports = connection;
